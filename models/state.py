6a679399d139a8668c6e39aec0e43e85d9ef34b8
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-09-17 13:29:52 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 """ State model """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """represent User class"""
-
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
