0ccc37213131eaa8ed89af30a94c89d2fa5fe945
beboo22 <mt557829@gmail.com>
2023-09-17 12:31:19 +0300

    task

--- models/user.py
+++ models/user.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """ User model """
-from models.base_model import BaseModel ,Base
+from models.base_model import BaseModel 
 import models
-from SQLAlchemy import column ,string 
+from sqlalchemy import Column, Integer, String 
 
 
 
 class User(BaseModel):
     """represent User class"""
 
-    email = column(string(128), nullable=False)
-    password = column(string(128), nullable=False)
-    first_name = column(string(128), nullable=True)
-    last_name = column(string(128), nullable=True)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
     __tablename__ = "users"
+    
