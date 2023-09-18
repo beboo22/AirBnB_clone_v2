93d24f3ff353d2254e1760026f1937163e16086d
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-09-18 11:06:05 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """ Review model """
-from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 
 class Review(BaseModel):
     """ Review class """
-
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
