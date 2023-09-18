336dc3a5c6dfe0a779abc8dbdf576ffd00ae623a
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-09-18 11:11:49 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """ Amenity model """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """represent Amenity class"""
-
-    name = ""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
