43d8895f1f687404c98c696488bb6a6df6aeaecc
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-09-18 17:31:44 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """ City model """
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
 from sqlalchemy import ForeignKey
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from models.place import Place
 
 
 class City(BaseModel, Base):
     """represent City class"""
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="cities")
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
