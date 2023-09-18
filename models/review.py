2033f4a97232b9c210822b1edd06ced7ddfa46ad
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-09-18 17:20:43 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -7,7 +7,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 class Review(BaseModel, Base):
     """ Review class """
-    __tablename__ = "reviews"
+    tablename = "reviews"
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
