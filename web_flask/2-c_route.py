f0858d7efcf2b9d91cef47141bd57e12df99d490
beboo22 <mt557829@gmail.com>
2023-10-21 15:07:56 +0300

    task

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -14,5 +14,15 @@ def hello():
     return "Hello HBNB!"
 
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def ctxt(text):
+    return 'c '+text.replace('_', ' ')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
