ab220e396fe6383288a4a1fbebac2426761b999f
beboo22 <mt557829@gmail.com>
2023-10-22 16:38:53 +0300

    task

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application
+"""
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardwn():
+    """closes the storage on teardown"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    hone = 'States'
+    states = storage.all("State").values()
+    return render_template('8-cities_by_states.html', hone=hone, states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
